10c4d88a1bcd3782f42c8330e7a9f1577e2d6a66
Hamilton Achilles <hamilton.zanette@terceiroslinx.com.br>
2021-02-23 10:39:30 -0300

    Moving Hello World Request Mapping.

--- src/main/java/com/meli/forecasts/weather/WeatherForecastApplication.java
+++ src/main/java/com/meli/forecasts/weather/WeatherForecastApplication.java
@@ -2,12 +2,22 @@ package com.meli.forecasts.weather;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 public class WeatherForecastApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WeatherForecastApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello Far Away Solar System!";
 	}
 
 }
--- src/main/java/com/meli/forecasts/weather/controller/WeatherForecastController.java
+++ src/main/java/com/meli/forecasts/weather/controller/WeatherForecastController.java
@@ -1,15 +1,22 @@
 package com.meli.forecasts.weather.controller;
 
-import org.springframework.stereotype.Controller;
+import com.meli.forecasts.weather.dto.PlanetEnum;
+import com.meli.forecasts.weather.dto.WeatherDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import static com.meli.forecasts.weather.dto.WeatherEnum.LLUVIA;
+
+@RestController
+@RequestMapping("/weather")
 public class WeatherForecastController {
 
-    @RequestMapping("/")
-    @ResponseBody
-    String home() {
-        return "Hello Far Away Solar System!";
+    @GetMapping(path = "/planet/{planet}/day/{day}")
+    public ResponseEntity<WeatherDto> getPayment(@PathVariable PlanetEnum planet, @PathVariable Integer day) {
+        WeatherDto weather = new WeatherDto(1, LLUVIA);
+        return ResponseEntity.ok(weather);
     }
 }
