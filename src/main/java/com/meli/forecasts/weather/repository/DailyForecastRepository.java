104b844cab67261b6a23089d9a07db4e5e5fa865
Hamilton Achilles <hamilton.zanette@terceiroslinx.com.br>
2021-02-27 00:43:32 -0300

    Sorting forecast search

--- src/main/java/com/meli/forecasts/weather/repository/DailyForecastRepository.java
+++ src/main/java/com/meli/forecasts/weather/repository/DailyForecastRepository.java
@@ -4,8 +4,11 @@ import com.meli.forecasts.weather.model.DailyForecast;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface DailyForecastRepository extends JpaRepository<DailyForecast, Integer> {
 
+    List<DailyForecast> findAllOrderByDayAsc();
 
 }
